e208eb33326490110801e69f1ce3d17552be99c1
=Aminou-92-03 <=mefti.amine92@gmail.com>
2022-09-12 16:16:01 +0200

    git commit

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -10,6 +12,7 @@ app.use(expressLayouts)
 app.use(express.static('public'))
  
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 // require('dotenv').config()
 
@@ -21,6 +24,6 @@ const db = mongoose.connection
 db.on('error', error => console.log('db error', error))
 db.once('open', ()=> console.log('connected to mongooose'))
 
-app.listen(process.env.PORT || 3000,()=>{
-    console.log('listen to PORT', process.env.PORT || 3000 );
+app.listen(process.env.PORT || 3300,()=>{
+    console.log('listen to PORT', process.env.PORT || 3300 );
 })
