e50c2a22cbe958e4fe90b962a0b8617cec396d4a
=Aminou-92-03 <=mefti.amine92@gmail.com>
2022-09-12 15:35:49 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+ 
+app.use('/', indexRouter)
+
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser : true})
+
+const db = mongoose.connection
+db.on('error', error => console.log('db error', error))
+db.once('open', ()=> console.log('connected to mongooose'))
+
+app.listen(process.env.PORT || process.env.PORT_RECETTE,()=>{
+    console.log('listen to PORT', process.env.PORT || process.env.PORT_RECETTE );
+})
